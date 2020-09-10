71aa45164b5a1b15cca4f8ed92550ce5ed182a14
CJ Davis <csdj92@gmail.com>
2020-09-10 21:55:05 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -20,7 +20,8 @@ def swap_elements(array)
  array[0], array[1], array [2] = array[0], array[2], array[1]
 end
 
-def reverse_array
+def reverse_array(array)
+  array.sort_by { |i| -i.length}
 end
 
 def kesha_maker
